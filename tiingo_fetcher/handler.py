b21bd2776bf01545288e394adfa9b35f4dd8aec4
David Zucker <davzucky@hotmail.com>
2019-05-01 16:26:48 +0800

    Add function to fetch and transfrom tiingo input

--- tiingo_fetcher/handler.py
+++ tiingo_fetcher/handler.py
@@ -1,13 +1,22 @@
+import csv
+import io
 import logging
 import os
-from typing import Dict, Any
+import sys
+from collections import OrderedDict
+from typing import Dict, Any, Iterator, List
 
+from tiingo import TiingoClient
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 AWS_S3_BUCKET = "aws_s3_bucket"
 AWS_REGION = "aws_region"
+
+config = {}
+config["session"] = True
+client = TiingoClient(config)
 
 
 def get_env_variable(env_var_name: str):
@@ -18,14 +27,52 @@ def get_env_variable(env_var_name: str):
 
     return variable
 
+
 def handler(event: Dict[str, Any], context):
-    bucket = get_env_variable(AWS_S3_BUCKET)
-    region = get_env_variable(AWS_REGION)
+    # bucket = get_env_variable(AWS_S3_BUCKET)
+    # region = get_env_variable(AWS_REGION)
 
     for record in event["Records"]:
-        message = get_bloomberg_message(record)
-        market_data = get_bloomberg_ticker_market_data(message.ticker)
-        if isinstance(market_data, BloombergMarketDataError):
-            process_market_data_error(market_data)
-        elif isinstance(market_data, BloombergFundMarketData):
-            process_market_data(market_data, bucket, region, message.file_path)
+        #     process_market_data(market_data, bucket, region, message.file_pa
+        pass
+        # message = get_bloomberg_message(record)
+        # market_data = get_bloomberg_ticker_market_data(message.ticker)
+        # if isinstance(market_data, BloombergMarketDataError):
+        #     process_market_data_error(market_data)
+        # elif isinstance(market_data, BloombergFundMarketData):
+
+
+def get_tiingo_market_data(
+    ticker: str,
+    frequency: str = "daily",
+    start_date: str = "1800-1-1",
+    end_date: str = None,
+) -> csv.DictReader:
+    csv_prices = client.get_ticker_price(
+        ticker, frequency=frequency, startDate=start_date, endDate=end_date, fmt="csv"
+    )
+    return csv.DictReader(io.StringIO(csv_prices))
+
+
+def transform_to_output(input_data: Dict[str, object]) -> Dict[str, object]:
+    return OrderedDict(
+        [
+            ("date", input_data["date"]),
+            ("close", input_data["adjClose"]),
+            ("high", input_data["adjHigh"]),
+            ("low", input_data["adjLow"]),
+            ("open", input_data["adjOpen"]),
+            ("volume", input_data["adjVolume"]),
+        ]
+    )
+
+
+def get_csv_text(
+    input_data: Iterator[Dict[str, object]],
+    csv_columns: List[str] = ["date", "close", "high", "low", "open", "volume"],
+) -> str:
+    output = io.StringIO()
+    writer = csv.DictWriter(output, fieldnames=csv_columns, quoting=csv.QUOTE_NONE)
+    writer.writeheader()
+    writer.writerows(input_data)
+    return output.getvalue()
--- tiingo_fetcher/tests/test_tiingo.py
+++ tiingo_fetcher/tests/test_tiingo.py
@@ -1,25 +1,59 @@
-import os
+from tiingo_fetcher.handler import (
+    get_tiingo_market_data,
+    transform_to_output,
+    get_csv_text,
+)
 
-from tiingo import TiingoClient
 
-api_key = os.environ.get("TIINGO_API_KEY")
-config = {}
+def test_transform_input_dict_to_new_format():
+    input_dic = {
+        "date": "2019-04-08",
+        "close": "200.1",
+        "high": "200.23",
+        "low": "196.34",
+        "open": "196.42",
+        "volume": "25881697",
+        "adjClose": "200.1",
+        "adjHigh": "200.23",
+        "adjLow": "196.34",
+        "adjOpen": "196.42",
+        "adjVolume": "25881697",
+        "divCash": "0.0",
+        "splitFactor": "1.0",
+    }
+    output_dic = transform_to_output(input_dic)
+    expected_result = {
+        "date": "2019-04-08",
+        "close": "200.1",
+        "high": "200.23",
+        "low": "196.34",
+        "open": "196.42",
+        "volume": "25881697",
+    }
+    assert expected_result == output_dic
 
-# To reuse the same HTTP Session across API calls (and have better performance), include a session key.
-config['session'] = True
 
-# If you don't have your API key as an environment variable,
-# pass it in via a configuration dictionary.
-config['api_key'] = api_key
-
-# Initialize
-client = TiingoClient(config)
-
+def test_write_dict_to_memory_string_csv():
+    input_dict = [
+        {
+            "date": "2019-04-08",
+            "close": "200.1",
+            "high": "200.23",
+            "low": "196.34",
+            "open": "196.42",
+            "volume": "25881697",
+        }
+    ]
+    output_str = get_csv_text(input_dict)
+    expected_str = (
+        "date,close,high,low,open,volume\r\n2019-04-08,200.1,200.23,"
+        "196.34,196.42,25881697\r\n"
+    )
+    assert expected_str == output_str
 
 
 def test_call_tiingo():
-    prices = client.get_ticker_price("AAPL", startDate="1800-1-1", frequency="daily", fmt="csv")
-
-    for price in prices:
-        print(price)
-    assert len(prices) < 0
+    prices = get_tiingo_market_data(
+        "AAPL", frequency="daily", start_date="2019-4-8", end_date="2019-4-12"
+    )
+    assert len([p for p in prices]) == 5
