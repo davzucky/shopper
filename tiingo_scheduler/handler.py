cb5bccc19822df2b7bb892e6d292e8f06962ed3b
David Zucker <davzucky@hotmail.com>
2019-06-02 11:39:18 +0800

    Add tiingoTicker fetcher

--- tiingo_scheduler/handler.py
+++ tiingo_scheduler/handler.py
@@ -1,15 +1,29 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import Dict, Any
+
+from dataclass_csv import DataclassReader
+
+
+def get_tiingo_tickers(tiingo_ticker_path: str):
+    with open(tiingo_ticker_path) as tiingo_tickers_csv:
+        tickers_datareader = DataclassReader(tiingo_tickers_csv, TiingoTicker)
+        tickers_datareader.map("assetType").to("asset_type")
+        tickers_datareader.map("priceCurrency").to("price_currency")
+        tickers_datareader.map("startDate").to("start_date")
+        tickers_datareader.map("endDate").to("end_date")
+        for ticker in tickers_datareader:
+            yield ticker
 
 
 @dataclass
-class TiingoTicker():
+class TiingoTicker:
     ticker: str
-    exchange: str
     asset_type: str
     price_currency: str
-    start_date: str
-    end_date: str
+    exchange: str = "Unknown"
+    start_date: str = "1800-01-01"
+    end_date: str = "2800-01-01"
+
 
 def handler(event: Dict[str, Any], context):
     pass
--- tiingo_scheduler/tests/test_tiingo_ticker_csv.py
+++ tiingo_scheduler/tests/test_tiingo_ticker_csv.py
@@ -1,21 +1,29 @@
 import os
-from dataclass_csv import DataclassReader
 
-from ..handler import TiingoTicker
+import pytest
+
+from ..handler import get_tiingo_tickers
 
 
-
-
-def test_read_empty_tiingo_ticker(tmpdir):
+@pytest.mark.parametrize(
+    "file_content,nb_rows",
+    [
+        ("", 0),
+        (
+            "000001,SHE,Stock,CNY,2007-08-30,2019-05-30\n000002,SHE,Stock,CNY,"
+            "2000-01-04,2019-05-30",
+            2,
+        ),
+        ("000001,SHE,Stock,CNY,2007-08-30,2019-05-30\n000003,SHE,Stock,CNY,,\n", 2),
+    ],
+)
+def test_read_empty_tiingo_ticker(tmpdir, file_content: str, nb_rows: int):
     filename = os.path.join(tmpdir.dirname, "tiingo_tickers.csv")
     header = "ticker,exchange,assetType,priceCurrency,startDate,endDate"
 
     with open(filename, "w") as h:
-        h.write(header)
+        h.write(f"{header}\n{file_content}")
 
-    with open(filename) as users_csv:
-        reader = DataclassReader(users_csv, User)
-        for row in reader:
-            print(row)
+    tickers = [t for t in get_tiingo_tickers(filename)]
 
-    assert 1 == 1
+    assert len(tickers) == nb_rows
