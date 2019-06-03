d807f645ea18a39a3a6ac63efce7709191d7d741
David Zucker <davzucky@hotmail.com>
2019-06-03 09:02:35 +0800

    Update os_helper to be a synlink

--- tiingo_fetcher/os_helpers.py
+++ tiingo_fetcher/os_helpers.py
@@ -1,18 +1,1 @@
-import os
-
-import daiquiri
-
-logger = daiquiri.getLogger(__name__)
-
-
-def get_env_variable(env_var_name: str):
-    variable = os.environ.get(env_var_name, None)
-    if variable is None:
-        logger.error(f"The environment variable {env_var_name} is not set.")
-        raise ReferenceError(f"The environment variable {env_var_name} is not set.")
-
-    return variable
-
-
-def get_env_variable_or_default(env_var_name: str, default: str):
-    return os.environ.get(env_var_name, default)
+../shared/os_helpers.py
