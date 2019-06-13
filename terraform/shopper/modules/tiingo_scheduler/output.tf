5ce750cc3750521645100b31406d4a661e79bff8
David Zucker <davzucky@hotmail.com>
2019-06-13 23:08:57 +0800

    add extra output

--- terraform/shopper/modules/tiingo_scheduler/output.tf
+++ terraform/shopper/modules/tiingo_scheduler/output.tf
@@ -2,5 +2,9 @@ output "function_name" {
   value = "${aws_lambda_function.tiingo_scheduler_lambda_function.function_name}"
 }
 
+output "lambda_arn" {
+  value = "${aws_lambda_function.tiingo_scheduler_lambda_function.arn}"
+}
+
 
 
