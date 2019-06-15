de6e5f3c5784d5b2b112ed7b40ed1b0026b25878
David Zucker <davzucky@hotmail.com>
2019-06-15 12:00:11 +0800

    Fix tiingo fetch to work with terraform 0.12

--- terraform/shopper/modules/tiingo_fetcher/main.tf
+++ terraform/shopper/modules/tiingo_fetcher/main.tf
@@ -9,14 +9,14 @@ resource "aws_lambda_function" "tiingo_fetcher_lambda_function" {
   filename      = "${local.zip_file_path}"
   role          = "${aws_iam_role.tiingo_fetcher_lambda.arn}"
   runtime       = "python3.7"
-  source_code_hash = "${base64sha256(file("${local.zip_file_path}"))}"
+  source_code_hash = filebase64sha256("${local.zip_file_path}")
   timeout = "60"
-  tags {
+  tags = {
     version = "${var.module_version}"
   }
 
   environment {
-    variables {
+    variables = {
       "AWS_S3_BUCKET" = "${var.s3_market_data_bucket}",
       "TIINGO_API_KEY" = "${var.tiingo_api_key}"
     }
@@ -25,7 +25,7 @@ resource "aws_lambda_function" "tiingo_fetcher_lambda_function" {
 
 resource "aws_lambda_event_source_mapping" "tiingo_fetcher_sqs_trigger" {
   batch_size        = 1
-  event_source_arn  = "${var.trigger_sqs_arn}"
+  event_source_arn  = var.trigger_sqs_arn
   enabled           = true
   function_name     = "${aws_lambda_function.tiingo_fetcher_lambda_function.arn}"
 }
