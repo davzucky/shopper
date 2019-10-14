db901f8e867f47feec3eba56304865f8bdc73452
David Zucker <davzucky@hotmail.com>
2019-10-14 12:50:13 +0800

    #188 update path to the package file

--- tests/shopper/terraform/main.tf
+++ tests/shopper/terraform/main.tf
@@ -1,6 +1,7 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.12.6"
 }
+
 
 provider "aws" {
   region  = var.aws_region
@@ -16,15 +17,35 @@ resource "aws_s3_bucket" "test_tiingo" {
 resource "aws_s3_bucket_object" "tickers" {
   bucket = aws_s3_bucket.test_tiingo.bucket
   key    = "static/tiingo_tickers.csv"
-  source = "./supported_tickers.csv"
+  source = "${path.module}/supported_tickers.csv"
+  depends_on  = [aws_s3_bucket_object.tiingo_fetcher_lambda_package,
+                 aws_s3_bucket_object.tiingo_scheduler_lambda_package]
+
+}
+
+resource "aws_s3_bucket_object" "tiingo_fetcher_lambda_package" {
+  bucket = aws_s3_bucket.test_tiingo.bucket
+  key    = "lambda/tiingo_fetcher/${var.environment}/package.zip"
+  source = "${path.module}/../../../packages/tiingo_fetcher.${var.environment}.zip"
+}
+
+resource "aws_s3_bucket_object" "tiingo_scheduler_lambda_package" {
+  bucket = aws_s3_bucket.test_tiingo.bucket
+  key    = "lambda/tiingo_scheduler/${var.environment}/package.zip"
+  source = "${path.module}/../../../packages/tiingo_scheduler.${var.environment}.zip"
 }
 
 module "shopper" {
-  source                   = "../../../"
+  source = "../../../"
+  shopper_global = {
+    version          = var.environment
+    environment      = var.environment
+    region           = var.aws_region
+    loging_level     = "DEBUG"
+    S3_lambda_bucker = aws_s3_bucket.test_tiingo.arn
+  }
+
   tiingo_tickers_file_path = aws_s3_bucket_object.tickers.key
-  loging_level             = "DEBUG"
-  environment              = var.environment
-  region                   = var.aws_region
   S3_market_data_bucket    = aws_s3_bucket.test_tiingo.bucket
   tiingo_api_key           = var.tiingo_api_key
 }
