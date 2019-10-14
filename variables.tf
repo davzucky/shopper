29e2bb0c0500b96d29e03c422e87c68020743a2e
David Zucker <davzucky@hotmail.com>
2019-10-14 12:42:12 +0800

    #188 update input variable of the module

--- variables.tf
+++ variables.tf
@@ -1,17 +1,34 @@
-variable "environment" {
-  type        = "string"
-  description = "Name of the environment where the module is deployed"
-}
+//variable "environment" {
+//  type        = "string"
+//  description = "Name of the environment where the module is deployed"
+//}
+//
+//variable "module_version" {
+//  type = "string"
+//  description = "version of the module"
+//  default = "local"
+//}
+//
+//variable "loging_level" {
+//  type        = "string"
+//  description = "Login level to set for all modules"
+//  default     = "INFO"
+//}
+//
+//variable "region" {
+//  type        = "string"
+//  description = "Name of the region where the market data bucket is located"
+//}
 
-variable "loging_level" {
-  type        = "string"
-  description = "Login level to set for all modules"
-  default     = "INFO"
-}
-
-variable "region" {
-  type        = "string"
-  description = "Name of the region where the market data bucket is located"
+variable "shopper_global" {
+  type = object({
+    version          = string
+    environment      = string
+    loging_level     = string
+    region           = string
+    S3_lambda_bucker = string
+  })
+  description = "Shopper shared global variables"
 }
 
 variable "S3_market_data_bucket" {
